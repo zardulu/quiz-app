1123d4d2c97647a9793fad58472b993ad18dfc5e
Anshul <61840818+zardulu@users.noreply.github.com>
2023-11-03 02:05:24 +0530

    Add start page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Question from './components/Question';
-import quizData from './data/quiz.json'; 
+import quizData from './data/quiz.json';
+import Start from './components/Start'; 
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -8,6 +9,7 @@ export default function App() {
   const [showResults, setShowResults] = useState(false);
   const [timeTaken, setTimeTaken] = useState(0);
   const [timer, setTimer] = useState(0);
+  const [started, setStarted] = useState(false); 
 
   let message;
 
@@ -21,13 +23,12 @@ export default function App() {
     message = "Outstanding!";
   }
 
-
   useEffect(() => {
     if(currentQuestion >= quizData.length) {
       setShowResults(true);
     }
   }, [currentQuestion]);
-  
+
   useEffect(() => {
     if (!showResults) {
       setTimer(10); // Time per question
@@ -45,7 +46,6 @@ export default function App() {
       return () => clearInterval(interval);
     }
   }, [currentQuestion]);
-  
 
   const handleAnswer = (isCorrect) => {
     if (isCorrect) {
@@ -66,44 +66,55 @@ export default function App() {
 
   return (
     <div className="bg-gray-900 h-screen">
-      
-      <div className="fixed top-4 right-4 text-white text-xl">
-        Score: {score}
-      </div>
 
-      {showResults && (
-        <div className="results text-white pt-20 max-w-md mx-auto">
-          <h1 className="text-4xl font-bold text-center mb-6">
-            Quiz Results
-          </h1>
-          
-          <p className="text-2xl text-center mb-4">
-            Your score is <span className="font-bold">{score}</span> out of {quizData.length}
-          </p>
-          <p className="text-2xl text-center mb-4">
-          Time taken: {timeTaken} seconds
-          </p>
-          <p className="text-2xl text-center">{message}</p>
-          
-          <button 
-            className="bg-blue-500 text-white py-3 px-6 mt-8 rounded block mx-auto"
-            onClick={restartQuiz}  
-          >
-            Restart Quiz
-          </button>
+      {!started && (
+        <Start onStart={() => setStarted(true)} />
+      )}
+
+      {started && (
+        // Here, wrap the elements in a parent div
+        <div>
+          <div className="fixed top-4 right-4 text-white text-xl">
+            Score: {score}
+          </div> 
+
+          {showResults && (
+            <div className="results text-white pt-20 max-w-md mx-auto">
+              <h1 className="text-4xl font-bold text-center mb-6">
+                Quiz Results
+              </h1> 
+              
+              <p className="text-2xl text-center mb-4">
+                Your score is <span className="font-bold">{score}</span> out of {quizData.length}
+              </p>
+              <p className="text-2xl text-center mb-4">
+                Time taken: {timeTaken} seconds
+              </p>
+              <p className="text-2xl text-center">{message}</p>
+              
+              <button 
+                className="bg-blue-500 text-white py-3 px-6 mt-8 rounded block mx-auto"
+                onClick={restartQuiz}  
+              >
+                Restart Quiz
+              </button>
+            </div>
+          )}
+
+          {!showResults && (
+            // Wrap the Question component in a parent div
+            <div>
+              <Question 
+                data={quizData[currentQuestion]}
+                questionNum={currentQuestion + 1}
+                totalQuestions={quizData.length} 
+                onAnswer={handleAnswer}
+                timer={timer}
+              />
+            </div>
+          )}
         </div>
       )}
-
-      {!showResults && (
-        <Question 
-          data={quizData[currentQuestion]}
-          questionNum={currentQuestion + 1}
-          totalQuestions={quizData.length} 
-          onAnswer={handleAnswer}
-          timer={timer}
-        />
-      )}
-
     </div>
   );
 }
